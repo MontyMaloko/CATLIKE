6f2f846bdc852cacffe1a7b3f0c4e5889b8b1ef8
Monty Maloko <malefonkana@boilerroom.co.za>
2025-02-06 14:02:45 +0200

    Update Ball.cs
    Top and bottom paddles work. AI works. Ball doesnt work. Fuck me

--- Assets/PaddleGraph/Scripts/Ball.cs
+++ Assets/PaddleGraph/Scripts/Ball.cs
@@ -5,26 +5,20 @@ using UnityEngine.UIElements;
 
 public class Ball : MonoBehaviour
 {
-    [SerializeField, Min(0f)] float _constantXSpeed = 8f, _constantYSpeed = 10f, extents = 0.5f; //?
+    [SerializeField, Min(0f)] float _constantXSpeed = 8f, _constantYSpeed = 10f, extents = 0.5f; 
     public float Extents =>extents; //?
     public Vector2 Position => position;  //?
     Vector2 position, _velocity;
 
-    public void UpdateVisualization()
-    {
-        transform.localPosition = new Vector3(position.x, 0f, position.y);
-    }
+    public void UpdateVisualization() => transform.localPosition = new Vector3(position.x,0f,position.y);
 
-    public void Move()
-    {
-        position += _velocity * Time.deltaTime;
-    }
+    public void Move() => position += _velocity * Time.deltaTime;
 
     public void StartNewGame()
     {
         position = Vector2.zero;
-        //UpdateVisualization();
-        _velocity=new Vector2(_constantXSpeed,-_constantYSpeed);
+        UpdateVisualization();
+        _velocity = new Vector2(_constantXSpeed, -_constantYSpeed);
     }
 
     public void BounceX(float boundary)
